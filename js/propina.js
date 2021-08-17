739eb23e1f63e46132ef1cb3c5d027d9fd0b8dc4
jorsuap <jorsuap@gmail.com>
2021-08-17 17:13:22 -0500

    master

--- js/propina.js
+++ js/propina.js
@@ -1,6 +1,3 @@
-
-var bill = parseFloat(document.getElementById("bill").value);
-var people = parseFloat(document.getElementById("number").value);
 const keynumber = document.getElementsByName("data-porcent");
 
 var tip;
@@ -10,22 +7,48 @@ var totalbillbyperson;
 var porcent1;
 
 keynumber.forEach(function (key) {
-    key.addEventListener('click', function () {
+    key.addEventListener('click', function(){
+        identificador = this.id;
+        identificador.classList.add("botonactive");
+        // alert(identificador);
         porcent1 = parseFloat(key.innerText);
     })
 });
+
 function porcentaje(porcent1){
+
     var bill = parseFloat(document.getElementById("bill").value);
     var people = parseFloat(document.getElementById("number").value);
-    tip = (bill * porcent1)/100;
-    totalbill = bill + tip;
-    tipbyperson = tip / people;
-    totalbillbyperson = totalbill / people;
-    actaulizardisplay();
-}
+    var borderalert = document.getElementById("number");
+    var msnealert = document.getElementsByClassName("msnerror")[0];
+    var reset = document.getElementById("reset");
 
-function actaulizardisplay() {
-    document.getElementById("total__tip").innerHTML = "$"+tipbyperson;
-    document.getElementById("total__pagar").innerHTML = "$"+totalbillbyperson;
+    if(isNaN(bill) || isNaN(people)) {
+        document.getElementById("total__tip").innerHTML = "$0.00";
+        event.defaultPrevented;
+    } else {
+       
+        reset.classList.add("disable");
+        document.getElementById("reset").disabled = false;
 
+        if (people <= 0) {
+            borderalert.classList.add("errorborder");
+            msnealert.classList.add("activemsnerror")
+            event.defaultPrevented;
+            
+        } else {
+            borderalert.classList.remove("errorborder");
+            msnealert.classList.remove("activemsnerror")
+    
+            tip = (bill * porcent1) / 100;
+            totalbill = bill + tip;
+            tipbyperson = tip / people;
+            totalbillbyperson = totalbill / people;
+            totalbillbyperson = totalbillbyperson.toFixed(2);
+            tipbyperson = tipbyperson.toFixed(2);
+    
+            document.getElementById("total__tip").innerHTML = "$" + tipbyperson;
+            document.getElementById("total__pagar").innerHTML = "$" + totalbillbyperson;
+        }
+    }
 }
